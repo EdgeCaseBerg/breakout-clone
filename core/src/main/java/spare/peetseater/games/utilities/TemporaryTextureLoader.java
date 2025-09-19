a86e9763db3de07ca5d39a8855b9ef803dc97e89
peetseater <peetseater@gmail.com>
2025-09-18 23:52:21 -0400

    Continuing fiddling with Temporary texture loader
    Can make it work, but hard to write down things and blog about stuff as
    I learn it sometims. Eyes, tired, can barely see screen...

--- core/src/main/java/spare/peetseater/games/utilities/TemporaryTextureLoader.java
+++ core/src/main/java/spare/peetseater/games/utilities/TemporaryTextureLoader.java
@@ -9,33 +9,46 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class TemporaryTextureLoader extends AsynchronousAssetLoader<TemporaryTexture, TemporaryTextureLoader.TemporaryTextureParam> {
     public static class TemporaryTextureParam extends AssetLoaderParameters<TemporaryTexture> {
         private final Color color;
-        public TemporaryTextureParam(Color color) {
+        private final int width;
+        private final int height;
+
+        public TemporaryTextureParam(Color color, int width, int height) {
             this.color = color;
+            this.width = width;
+            this.height = height;
+        }
+
+        public TemporaryTextureParam(Color color) {
+            this(color, 10, 10);
         }
     }
-
 
     public TemporaryTextureLoader(FileHandleResolver resolver) {
         super(resolver);
     }
 
+    Pixmap pixmap;
     @Override
     public void loadAsync(AssetManager assetManager, String filename, FileHandle file, TemporaryTextureParam parameter) {
+        pixmap = null;
+        pixmap = new Pixmap(parameter.width, parameter.height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(parameter.color.cpy());
+        pixmap.fill();
     }
 
     @Override
     public TemporaryTexture loadSync(AssetManager manager, String fileName, FileHandle file, TemporaryTextureParam parameter) {
-        Pixmap pixmap = new Pixmap(10, 10, Pixmap.Format.RGBA8888);
-        pixmap.setColor(parameter.color.cpy());
-        pixmap.fill();
+        Pixmap pixmap = this.pixmap;
         Texture tmp = new Texture(pixmap);
         TemporaryTexture texture = new TemporaryTexture(tmp);
         pixmap.dispose();
+        this.pixmap = null;
         tmp.dispose();
         return texture;
     }
