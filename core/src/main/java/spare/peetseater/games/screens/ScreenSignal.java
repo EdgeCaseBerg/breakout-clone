b6d4fbf9f3b4a37f3f13b24ad970dba27a063fde
peetseater <peetseater@gmail.com>
2025-09-21 16:31:17 -0400

    documentation

--- core/src/main/java/spare/peetseater/games/screens/ScreenSignal.java
+++ core/src/main/java/spare/peetseater/games/screens/ScreenSignal.java
@@ -1,7 +1,19 @@
 package spare.peetseater.games.screens;
 
+import spare.peetseater.games.BustOutRun;
+
 public enum ScreenSignal {
+    /** Signal to the game runner that a queued scene should be loaded
+     * The current screen will _not_ be disposed of and will remain in
+     * memory with the expectation that it will be used again soon.
+     * Such as a pause menu or similar.
+     **/
     OVERLAY_SCENE,
+    /** Signal to the game runner to unload the current scene and display the next in queue
+     * It is the responsibility of the current scene to ensure that there is a queued scene
+     * to be transitioned to. Use {@link BustOutRun}'s requestSceneChangeTo method to do so.
+     * */
     UNLOAD,
+    /** Continue rendering the current scene */
     CONTINUE,
 }
