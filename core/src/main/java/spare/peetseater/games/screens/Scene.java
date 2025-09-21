183051100d0d81ebd246b2c8e16a83f80ff9dea1
peetseater <peetseater@gmail.com>
2025-09-20 21:20:24 -0400

    Clean up some imports

--- core/src/main/java/spare/peetseater/games/screens/Scene.java
+++ core/src/main/java/spare/peetseater/games/screens/Scene.java
@@ -1,8 +1,6 @@
 package spare.peetseater.games.screens;
 
 import com.badlogic.gdx.Screen;
-import spare.peetseater.games.utilities.SceneAssetBundle;
-import spare.peetseater.games.utilities.SceneAssetBundleLoader;
 
 public interface Scene {
     public String getBundleName();
