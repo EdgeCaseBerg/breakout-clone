7bbde82e40146e89935cdf5ebe1029a1f3a39f7f
peetseater <peetseater@gmail.com>
2025-09-21 15:16:55 -0400

    Update game loop to have queue for loading

--- core/src/main/java/spare/peetseater/games/BustOutRun.java
+++ core/src/main/java/spare/peetseater/games/BustOutRun.java
@@ -4,9 +4,17 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import spare.peetseater.games.screens.LevelScreen;
 import spare.peetseater.games.screens.Scene;
+import spare.peetseater.games.screens.ScreenSignal;
+import spare.peetseater.games.screens.transitions.FadeOut;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class BustOutRun extends Game {
@@ -14,23 +22,26 @@ public class BustOutRun extends Game {
     public SpriteBatch batch;
     public AssetManager assetManager;
     public Scene loadingScreen;
-    public Scene desiredScreen;
+    public Scene currentScreen;
+    private Queue<Scene> toLoad;
 
     @Override
     public void create() {
+        toLoad = new ConcurrentLinkedQueue<>();
         assetManager = new AssetManager();
         GameAssets.configure(assetManager);
         batch = new SpriteBatch();
         loadingScreen = new InitialLoadingScreen(this);
-        desiredScreen = new LevelScreen(this);
+        currentScreen = loadingScreen;
         setScreen(loadingScreen.getScreen());
+        requestSceneChangeTo(new LevelScreen(this));
     }
 
     @Override
     public void dispose() {
         super.dispose();
         loadingScreen.getScreen().dispose();
-        desiredScreen.getScreen().dispose();
+        currentScreen.getScreen().dispose();
         assetManager.dispose();
     }
 
@@ -47,12 +58,78 @@ public class BustOutRun extends Game {
             }
         }
 
-        if (assetManager.isLoaded(desiredScreen.getBundleName())) {
-            if (getScreen().equals(loadingScreen.getScreen())) {
-                setScreen(desiredScreen.getScreen());
-            }
+        float delta = Gdx.graphics.getDeltaTime();
+        ScreenSignal signal = currentScreen.update(delta);
+        switch (signal) {
+            case OVERLAY_SCENE:
+                loadSceneOnSignal();
+                break;
+            case UNLOAD:
+                if (toLoad.isEmpty()) {
+                    Gdx.app.log("SCENES", "asked to unload current scene but there is no scene to return to");
+                    throw new RuntimeException("...");
+                }
+                Scene nextScene = toLoad.peek();
+                if (assetManager.isLoaded(nextScene.getBundleName())) {
+                    if (loadingScreen != currentScreen) {
+                        assetManager.unload(currentScreen.getBundleName());
+                        currentScreen.getScreen().dispose();
+                    }
+                    currentScreen = toLoad.poll();
+                    setScreen(currentScreen.getScreen());
+                } else {
+                    currentScreen = loadingScreen;
+                    if (!getScreen().equals(loadingScreen.getScreen())) {
+                        setScreen(loadingScreen.getScreen());
+                    }
+                }
+
+                break;
+            case CONTINUE:
+            default:
+                if (screen != null) screen.render(Gdx.graphics.getDeltaTime());
         }
-        super.render();
+
     }
 
+    private void loadSceneOnSignal() {
+        if (toLoad.isEmpty()) {
+            Gdx.app.log("SCENES", "asked to load next scene but there is none queued");
+            throw new RuntimeException("...");
+        }
+
+        Scene nextScene = toLoad.peek();
+        if (assetManager.isLoaded(nextScene.getBundleName())) {
+            currentScreen = toLoad.poll();
+            setScreen(currentScreen.getScreen());
+        } else {
+            currentScreen = loadingScreen;
+            if (!getScreen().equals(loadingScreen.getScreen())) {
+                setScreen(loadingScreen.getScreen());
+            }
+        }
+    }
+
+    public void requestSceneChangeTo(Scene scene) {
+        // When we do screen transitions, this is where we can
+        // queue up a FadeOut, Load, FadeIn, Desired Scene, list
+        // of values once we make those.
+        toLoad.add(new FadeOut(this, 2f, screenshot()));
+        toLoad.add(scene);
+    }
+
+    private Texture screenshot() {
+        FrameBuffer frameBuffer = new FrameBuffer(
+            Pixmap.Format.RGBA8888,
+            Gdx.graphics.getWidth(),
+            Gdx.graphics.getHeight(),
+            false
+        );
+        batch.flush();;
+        frameBuffer.begin();
+        currentScreen.getScreen().render(0);
+        frameBuffer.end();
+        Texture screenshot = frameBuffer.getColorBufferTexture();
+        return screenshot;
+    }
 }
