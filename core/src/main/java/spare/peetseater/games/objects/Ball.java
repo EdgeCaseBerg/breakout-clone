3a66887349012468ab8f02808750c526b042883f
peetseater <peetseater@gmail.com>
2025-09-28 19:38:23 -0400

    5/8 unit test passing for withIntersection
    We're getting closer. but must deal with colinearness. (THE LINES BE
    TOUCHIN)

--- core/src/main/java/spare/peetseater/games/objects/Ball.java
+++ core/src/main/java/spare/peetseater/games/objects/Ball.java
@@ -1,6 +1,8 @@
 package spare.peetseater.games.objects;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class Ball {
@@ -14,7 +16,47 @@ public class Ball {
         this.dimensions = new Vector2(10, 10);
     }
 
+    /*
+     * https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
+     */
+    private boolean counterclockwise(Vector2 a, Vector2 b, Vector2 c) {
+        return (c.y - a.y) * (b.x - a.x) > (b.y - a.y) * (c.x - a.x);
+    }
+
+    private boolean intersect(Vector2 a, Vector2 b, Vector2 c, Vector2 d) {
+        return counterclockwise(a,c,d) != counterclockwise(b,c,d) && counterclockwise(a,b,c) != counterclockwise(a,b,d);
+    }
+
     public boolean willIntersect(Obstacle obstacle, float time) {
+        // Our expected line segment from the ball's anchor to the anchor at time t.
+        // we compute this from each corner through the walls of the obstacle
+        float bw = getDimensions().x;
+        float bh = getDimensions().y;
+        Vector2[] corners = {
+          getPosition(),
+          getPosition().add(0, bh),
+          getPosition().add(bw, 0 ),
+          getPosition().add(getDimensions())
+        };
+
+        for (int i = 0; i < corners.length; i++) {
+            Vector2 ballStart = corners[i];
+            float endX = ballStart.x + velocity.x * time;
+            float endY = ballStart.y + velocity.y * time;
+            Vector2 ballEnd = new Vector2(endX, endY);
+            // Ball XY to End XY is now a line segment that we need to check if it
+            // passes through any of the walls of the obstacle.
+            float ow = obstacle.getDimensions().x;
+            float oh = obstacle.getDimensions().y;
+
+            boolean intersectsBottom = intersect(ballStart, ballEnd, obstacle.getPosition(), obstacle.getPosition().add(ow, 0));
+            boolean intersectsLeftWall = intersect(ballStart, ballEnd, obstacle.getPosition(), obstacle.getPosition().add(0, oh));
+            boolean intersectsTop = intersect(ballStart, ballEnd, obstacle.getPosition().add(0, oh), obstacle.getPosition().add(ow, oh));
+            boolean intersectsRightWall = intersect(ballStart, ballEnd, obstacle.getPosition().add(ow, 0), obstacle.getPosition().add(ow, oh));
+            if ( intersectsBottom || intersectsLeftWall || intersectsTop || intersectsRightWall) {
+                return true;
+            }
+        }
         return false;
     }
 
