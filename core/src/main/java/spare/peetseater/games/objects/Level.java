57e8a4803a2aa0d08c3421b0ebcc83e9c4d83416
peetseater <peetseater@gmail.com>
2025-10-05 17:03:36 -0400

    Update level to check collisions

--- core/src/main/java/spare/peetseater/games/objects/Level.java
+++ core/src/main/java/spare/peetseater/games/objects/Level.java
@@ -1,7 +1,10 @@
 package spare.peetseater.games.objects;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Level {
@@ -15,10 +18,49 @@ public class Level {
         this.ball = new Ball(
             this.player.getPosition()
                 .add(player.getDimensions().x/2f, player.getDimensions().y)
-            , Vector2.Zero
+            , Vector2.Zero.cpy()
         );
         this.obstacles = obstacles;
         this.ballSpeed = ballSpeed;
+    }
+
+    public void launchBall(Vector2 velocity) {
+        this.ball.setVelocity(velocity);
+    }
+
+    private void collideBallWithObstacle(Obstacle obstacle, float delta) {
+        Vector2 ov = obstacle.getVelocity();
+        Vector2 reflection = ball.getCenter().sub(obstacle.getCenter());
+        reflection.x = Math.signum(reflection.x);
+        reflection.y = Math.signum(reflection.y);
+        Vector2 bv = this.ball.getVelocity();
+        bv.x = Math.abs(bv.x);
+        bv.y = Math.abs(bv.y);
+        Vector2 v = bv.scl(reflection).add(ov);
+        this.ball.setVelocity(v);
+        this.ball.update(delta);
+    }
+
+    public void update(float delta) {
+        this.player.update(delta);
+        boolean ballHitSomething = false;
+        // Obstacle collisions
+        for (Obstacle obstacle : obstacles) {
+            if (this.ball.willIntersect(obstacle, delta)) {
+                collideBallWithObstacle(obstacle, delta);
+                ballHitSomething = true;
+                break;
+            }
+        }
+
+        // Paddle collisions
+        if (this.ball.willIntersect(player, delta)) {
+            collideBallWithObstacle(player, delta);
+            ballHitSomething = true;
+        }
+        if (!ballHitSomething) {
+            this.ball.update(delta);
+        }
     }
 
     public Ball getBall() {
