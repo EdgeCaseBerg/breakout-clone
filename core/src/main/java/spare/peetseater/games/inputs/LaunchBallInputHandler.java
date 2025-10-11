11f841f0b8d811e3f8f25e279cc0cc361acc5fd7
peetseater <peetseater@gmail.com>
2025-10-11 00:26:41 -0400

    Launch ball with LaunchBallHandler

--- core/src/main/java/spare/peetseater/games/inputs/LaunchBallInputHandler.java
+++ core/src/main/java/spare/peetseater/games/inputs/LaunchBallInputHandler.java
@@ -1,4 +1,31 @@
 package spare.peetseater.games.inputs;
 
-public class LaunchBallInputHandler {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import spare.peetseater.games.objects.Level;
+
+public class LaunchBallInputHandler extends PaddleInputHandler {
+
+    final Level level;
+    final Vector2 launchVector;
+    private boolean isLaunched;
+
+    public LaunchBallInputHandler(Level level) {
+        super(level.getPlayer());
+        this.level = level;
+        this.launchVector = new Vector2(1, 1);
+        this.isLaunched = false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.SPACE) {
+            this.isLaunched = true;
+            level.launchBall(launchVector.cpy().scl(50));
+            return true;
+        }
+        return super.keyDown(keycode);
+    }
 }
