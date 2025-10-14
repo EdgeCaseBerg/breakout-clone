e38c6978015b1a1b88b410612298243e641bc189
peetseater <peetseater@gmail.com>
2025-10-13 22:05:45 -0400

    Port over true type loaders for fonts

--- core/src/main/java/spare/peetseater/games/GameAssets.java
+++ core/src/main/java/spare/peetseater/games/GameAssets.java
@@ -1,8 +1,13 @@
 package spare.peetseater.games;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
+import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
 import spare.peetseater.games.screens.LevelScreenBundleParams;
 import spare.peetseater.games.screens.transitions.FadeOutBundleParams;
 import spare.peetseater.games.screens.transitions.LoadingScreenBundleParams;
@@ -11,11 +16,16 @@ import spare.peetseater.games.utilities.SceneAssetBundleLoader;
 import spare.peetseater.games.utilities.TemporaryTexture;
 import spare.peetseater.games.utilities.TemporaryTextureLoader;
 
+import static spare.peetseater.games.GameRunner.GAME_HEIGHT;
+import static spare.peetseater.games.GameRunner.GAME_WIDTH;
+
 public class GameAssets {
 
     public static void configure(AssetManager assetManager) {
         assetManager.setLoader(TemporaryTexture.class, new TemporaryTextureLoader(assetManager.getFileHandleResolver()));
         assetManager.setLoader(SceneAssetBundle.class, new SceneAssetBundleLoader(assetManager.getFileHandleResolver()));
+        assetManager.setLoader(FreeTypeFontGenerator.class, new FreeTypeFontGeneratorLoader(assetManager.getFileHandleResolver()));
+        assetManager.setLoader(BitmapFont.class, ".ttf", new FreetypeFontLoader(assetManager.getFileHandleResolver()));
     }
 
     public static final String PLAYER_ASSET_KEY = "player";
@@ -39,4 +49,23 @@ public class GameAssets {
     public static final String LOADING_SCREEN_BUNDLE_KEY = "loadingscreen.bundle";
     public static final AssetDescriptor<SceneAssetBundle> LOADING_SCREEN_BUNDLE = new AssetDescriptor<>(LOADING_SCREEN_BUNDLE_KEY, SceneAssetBundle.class, new LoadingScreenBundleParams());
 
+    public static final String SCORE_FONT_KEY = "visitor50.ttf";
+    public static final AssetDescriptor<BitmapFont> scoreFont = visitorFontDescriptor(50, SCORE_FONT_KEY);
+
+    public static AssetDescriptor<BitmapFont> visitorFontDescriptor(int sizeInPixels, String key) {
+        FreetypeFontLoader.FreeTypeFontLoaderParameter params = new FreetypeFontLoader.FreeTypeFontLoaderParameter();
+        params.fontFileName = "visitor/visitor1.ttf";
+        params.fontParameters.size = sizeInPixels;
+        return new AssetDescriptor<>(key, BitmapFont.class, params);
+    }
+
+    static public BitmapFont getScaledFont(AssetDescriptor<BitmapFont> key, AssetManager assetManager) {
+        BitmapFont font = assetManager.get(key);
+        font.setUseIntegerPositions(false);
+        font.getData().setScale(
+            (float) GAME_WIDTH / Gdx.graphics.getWidth(),
+            (float) GAME_HEIGHT / Gdx.graphics.getHeight()
+        );
+        return font;
+    }
 }
