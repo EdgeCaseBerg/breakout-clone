536e4dd2be3602e9d1944daa31a55ad4881346bd
peetseater <peetseater@gmail.com>
2025-10-17 17:51:29 -0400

    Fill out TimedAction
    I really hate how intelliji stages new files automatically, I didn't
    mean tto commit the starter bit of this a few commits back but I'm also
    currently too lazy to rebase interactive and remove it

--- core/src/main/java/spare/peetseater/games/screens/transitions/TimedAction.java
+++ core/src/main/java/spare/peetseater/games/screens/transitions/TimedAction.java
@@ -1,4 +1,29 @@
 package spare.peetseater.games.screens.transitions;
 
+import java.util.function.Supplier;
+
 public class TimedAction {
+    private float accum;
+    private final Supplier<Boolean> action;
+    private float period;
+    private boolean done;
+
+    public TimedAction(float period, Supplier<Boolean> action) {
+        this.period = period;
+        this.accum = 0;
+        this.action = action;
+        this.done = false;
+    }
+
+    public void update(float secondsSince) {
+        accum += secondsSince;
+        if (accum > period) {
+            accum -= period;
+            done = action.get();
+        }
+    }
+
+    public boolean isDone() {
+        return done;
+    }
 }
